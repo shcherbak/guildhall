30d92a1ef7e68b721736b8d42cb192c73447dc27
Yurii Shcherbak <shcherbak@localhost.localdomain>
2017-05-31 14:41:50 +0300

    initial commit

---
+++ db/mes.sql
@@ -0,0 +1,12 @@
+-- Database: mes
+
+-- DROP DATABASE mes;
+
+CREATE DATABASE mes
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'uk_UA.UTF-8'
+       LC_CTYPE = 'uk_UA.UTF-8'
+       CONNECTION LIMIT = -1;
+
